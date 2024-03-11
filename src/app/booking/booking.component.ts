486ad016676ed632d36ba5cdee19953cfd5bcf00
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-03-11 16:12:10 +0000

    Google Pay added

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { GooglePayButtonModule } from '@google-pay/button-angular'
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
+  imports: [GooglePayButtonModule],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css'
 })
 export class BookingComponent {
+  buttonWidth = 240
+  paymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          allowedCardNetworks: ["AMEX", "VISA", "MASTERCARD"]
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId'
+          }
+        }
+      }
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant'
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '100.00',
+      currencyCode: 'USD',
+      countryCode: 'US'
+    }
+  }
 
+  onLoadPaymentData(event: any) {
+    console.log(event, "DATA");
+    
+  }
 }
