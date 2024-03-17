fab18752f86727262e11d324001ebc7e19b3d8be
Junaid Ahmed <ja894483@gmail.com>
2024-03-17 23:19:18 +0000

    temporary working signup/login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { AuthService } from './auth.service';
+import { inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { UserInterface } from './user.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  authService = inject(AuthService)
+  http = inject(HttpClient)
   title = 'ox-front';
+
+  ngOnInit(): void {
+    this.http.get<{ user: UserInterface }>('https://api.realworld.io/api/user')
+    .subscribe({
+      next: (response) => {
+        console.log('response', response);
+        this.authService.currentUserSig.set(response.user);
+      },
+      error: () => {
+        this.authService.currentUserSig.set(null);
+      }
+    });
+  }
+
+  logout(): void {
+    console.log("logout");
+    localStorage.setItem('token', '')
+    this.authService.currentUserSig.set(null);
+  }
 }
