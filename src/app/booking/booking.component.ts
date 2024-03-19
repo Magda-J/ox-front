2b8863a5ae94c23e9747633e0413084c9aa568c6
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-03-19 14:56:49 +0000

    changes

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -52,15 +52,6 @@ export class BookingComponent {
     
   }
   
-  // createFormObject = () => {
-  //   const firstName = document.querySelector('#first-name') as HTMLInputElement
-  //   const lastName = document.querySelector('#last-name') as HTMLInputElement
-  //   const email = document.querySelector('#email') as HTMLInputElement
-  //   const phone = document.querySelector('#phone') as HTMLInputElement
-  //   const address = document.querySelector('#address') as HTMLInputElement
-  //   const city = document.querySelector('#city') as HTMLInputElement
-  //   const postcode = document.querySelector('#postcode') as HTMLInputElement
-
     formData: ParticipantInterface = {
       _id: '',
       firstName: '',
@@ -72,21 +63,7 @@ export class BookingComponent {
       postcode: ''
     }
 
-    // console.log(formData);
-
-  // }
-
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
-
-  // ngOnInit(): void {
-  //   this.route.params.subscribe(params => {
-  //     const eventid = params['eventid'];
-  //     const dateindex = params['dateindex'];
-  //     // Now you can use eventid and dateindex as needed
-  //     console.log('Event ID:', eventid);
-  //     console.log('Date Index:', dateindex);
-  //   });
-  // }
 
   onClick(): void {
 
